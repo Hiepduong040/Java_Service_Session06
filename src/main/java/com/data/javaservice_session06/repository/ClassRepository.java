a1e699138de864ab8cc985b358bc6dd48c17ac46
Hiepduong040 <hiepduong0409@gmail.com>
2025-07-16 11:02:31 +0700

    add

--- src/main/java/com/data/javaservice_session06/repository/ClassRepository.java
+++
@@ -1,9 +0,0 @@
-// ClassRepository.java
-package com.data.javaservice_session06.repository;
-
-import com.data.javaservice_session06.model.entity.Classes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassRepository extends JpaRepository<Classes, Integer> {
-    Classes findByClassName(String className);
-}
---
+++ src/main/java/com/data/session06/JavaServiceSession06Application.java
@@ -0,0 +1,13 @@
+package com.data.session06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServiceSession06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaServiceSession06Application.class, args);
+    }
+
+}
